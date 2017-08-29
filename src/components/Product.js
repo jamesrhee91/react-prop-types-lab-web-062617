47d35ef3a2a87b4f2d64ee66f15225c697574a63
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-28 22:17:49 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,48 @@
-// Code Product Component Here
+import React from 'react'
+import PropTypes from 'prop-types'
+import { createPropType } from 'react-custom-proptypes';
+
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <h4>Producer: {this.props.producer}</h4>
+        <h4>{this.props.hasWatermark}</h4>
+        <h4>Color: {this.props.color}</h4>
+        <h4>Weight: {this.props.weight}</h4>
+      </div>
+    )
+  }
+}
+
+function rangePropType(props, propName, componentName) {
+  if (props[propName] === undefined) {
+    return new Error('Required')
+  } else if (isNaN(props[propName])) {
+    return new Error('Must be a number')
+  } else if (props[propName] > 300 || props[propName] < 80) {
+    return new Error('Must be in between 80 - 300')
+  }
+}
+// const rangePropType = createPropType(prop =>
+//   Number.isInteger(prop) &&
+//   prop >= 80 &&
+//   prop <= 300,
+//   'Must be between 80 - 300'
+// )
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: rangePropType
+}
+
+export default Product
